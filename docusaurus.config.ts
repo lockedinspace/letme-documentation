d533a7072a43eb55cde0e9897869b17aec9fe1f2
gonzalo <gonzalo.vidal@nubersia.com>
2024-07-18 13:49:57 +0200

    updated domains

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/letme.ico',
 
   // Set the production url of your site here
-  url: 'https://lockedinspace.com',
+  url: 'https://www.getletme.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/letme',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
